f723c77a98ae255b90f401e7363c45e39d962ce1
javiercarpio57 <javierrr.carpio57@gmail.com>
2018-02-02 11:51:35 -0600

    Puse "hola"

---
+++ HojaTrabajo3/src/hojatrabajo3/HojaTrabajo3.java
@@ -0,0 +1,18 @@
+
+package hojatrabajo3;
+
+/**
+ *
+ * @author javie
+ */
+public class HojaTrabajo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int hola = 0;
+    }
+    
+}
