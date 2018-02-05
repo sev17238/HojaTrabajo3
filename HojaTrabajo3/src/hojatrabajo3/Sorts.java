e3a6bf342ab128d362d760ed7e2266f8333d945a
javiercarpio57 <javierrr.carpio57@gmail.com>
2018-02-05 12:00:16 -0600

    Agregue clase sorts

---
+++ HojaTrabajo3/src/hojatrabajo3/Sorts.java
@@ -0,0 +1,26 @@
+
+package hojatrabajo3;
+
+/**
+ *
+ * @author javie
+ */
+public class Sorts {
+    
+    public void GnomeSort(){
+        
+    }
+    public void MergeSort(){
+        
+    }
+    public void QuickSort(){
+        
+    }
+    public void RadixSort(){
+        
+    }
+    public void otro(){
+        
+    }
+    
+}
